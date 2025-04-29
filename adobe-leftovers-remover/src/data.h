db6fd35e3d5f20b3d730152b88f92cb21a29dd70
ExoticGamerrrYT <carlosalfaromolina27@gmail.com>
2025-04-29 14:44:46 +0200

    updated dirs

--- adobe-leftovers-remover/src/data.h
+++ adobe-leftovers-remover/src/data.h
@@ -1,16 +1,41 @@
 #pragma once
-
 #include <QStringList>
 
-// List of paths (directories)
 inline const QStringList paths = {
-	"C:\\Route1",
-	"D:\\Backup",
-	"E:\\Files"
+    "C:/Program Files/Adobe",
+    "C:/Program Files/Common Files/Adobe",
+    "C:/Program Files (x86)/Adobe",
+    "C:/Program Files (x86)/Common Files/Adobe",
+    "C:/ProgramData/Adobe",
+    "%LOCALAPPDATA%/Adobe",
+    "%LOCALAPPDATA%/Low/Adobe",
+    "%APPDATA%/Adobe",
+    "%TEMP%/Adobe",
+    "%USERPROFILE%/Documents/Adobe",
+    "%ALLUSERSPROFILE%/Adobe",
+    "%ProgramData%/Adobe/SLStore",
+    "%ProgramData%/Adobe/SLCache",
+    "%LOCALAPPDATA%/Adobe/Creative Cloud Libraries",
+    "%LOCALAPPDATA%/Adobe/Creative Cloud Desktop",
+    "%APPDATA%/Adobe/Creative Cloud Libraries",
+    "%APPDATA%/Adobe/CoreSync",
+    "%APPDATA%/Adobe/CCX Welcome",
+    "%APPDATA%/Adobe/OOBE"
 };
 
-// List of registry keys
 inline const QStringList registryKeys = {
-	"HKEY_LOCAL_MACHINE\\Software\\App",
-	"HKEY_CURRENT_USER\\Settings"
+    "HKEY_CURRENT_USER\\Software\\Adobe",
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe",
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Adobe",
+    "HKEY_CLASSES_ROOT\\Adobe",
+    "HKEY_CURRENT_USER\\Software\\Adobe\\Photoshop",
+    "HKEY_CURRENT_USER\\Software\\Adobe\\Acrobat Reader",
+    "HKEY_CURRENT_USER\\Software\\Adobe\\After Effects",
+    "HKEY_CURRENT_USER\\Software\\Adobe\\Common",
+    "HKEY_CURRENT_USER\\Software\\Adobe\\Premiere Pro",
+    "HKEY_CURRENT_USER\\Software\\Adobe\\Creative Cloud",
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Adobe Acrobat",
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Adobe Creative Cloud",
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\CSXS.*",
+    "HKEY_LOCAL_MACHINE\\Software\\MozillaPlugins\\Adobe Acrobat"
 };
